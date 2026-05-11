f558f41e767e6a59095b4e55c4903a76bd3af4e5
zhulimeng-ai <zlmistc@gmail.com>
2026-05-11 15:16:09 +0800

    Add main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from src.ui.main_window import MainWindow
+
+
+def main():
+    app = QApplication(sys.argv)
+    app.setStyle('Fusion')
+    
+    window = MainWindow()
+    window.show()
+    
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
